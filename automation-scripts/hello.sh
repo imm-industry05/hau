ff9338eac8efbfe921747b642053189823a20e1d
imrmendez <imm.industry05@gmail.com>
2022-09-06 18:30:37 +0800

    from test branch

--- automation-scripts/hello.sh
+++ automation-scripts/hello.sh
@@ -2,4 +2,7 @@
 #
 #
 
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, Wordpress!"}' 
+url='https://hooks.slack.com/services/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'
+msg=${1}
+
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, '${msg}'!"}' ${url}
