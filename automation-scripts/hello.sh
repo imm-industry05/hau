95b3019dca657aa733ff5273ceaf5f60962a3ddd
imrmendez <imm.industry05@gmail.com>
2022-09-07 15:09:22 +0800

    update hello.sh

--- automation-scripts/hello.sh
+++ automation-scripts/hello.sh
@@ -5,4 +5,18 @@
 url=${1}
 msg=${2}
 
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, '${msg}'!"}' ${url}
+# curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, '${msg}'!"}' ${url}
+
+webserver() {
+    docker ps -a | grep node-inventory-app
+}
+
+# webapp() {
+
+# }
+
+# db() {
+
+# }
+
+webserver
