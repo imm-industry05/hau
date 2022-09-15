47da67ecf7b5407c3e8dd5400a698fab603cfda3
unknown <patricksantos2627@gmail.com>
2022-09-15 22:35:58 +0800

    new and update

--- santos-p-RGW2(Responsive Gaming Website)/js/main.js
+++ santos-p-RGW2(Responsive Gaming Website)/js/main.js
@@ -15,3 +15,31 @@ document.querySelector('#bell-icon').onclick = () =>{
     bell.classList.toggle('active');
 } 
 
+
+// Swiper
+var swiper = new Swiper(".trending-content", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+        delay:5000,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 15,
+      },
+      1068: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    },
+  });
