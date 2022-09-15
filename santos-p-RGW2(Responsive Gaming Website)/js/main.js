5644246deca5958175503db2830dcfafd80c92e7
unknown <patricksantos2627@gmail.com>
2022-09-15 17:58:47 +0800

    new and update

--- santos-p-RGW2(Responsive Gaming Website)/js/main.js
+++ santos-p-RGW2(Responsive Gaming Website)/js/main.js
@@ -4,4 +4,13 @@ let navbar = document.querySelector('.menu');
 menu.onclick = () => {
     navbar.classList.toggle('active');
     menu.classList.toggle('move');
+    
 }
+
+// notification
+let bell = document.querySelector('.notification');
+
+document.querySelector('#bell-icon').onclick = () =>{
+    bell.classList.toggle('active');
+} 
+
