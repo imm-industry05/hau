ce7b5bfe067c8ba722d283732aad529070612c8b
bondoc-jd <jandenielbondocbondoc@gmail.com>
2022-09-15 12:22:17 +0800

    Inventory System

--- bondoc-jd-inventory/InventorySystem/adminPage.php
+++ bondoc-jd-inventory/InventorySystem/adminPage.php
@@ -7,8 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
-    <link rel="stylesheet" type="text/css" href="./homePage2.css">
-    <link rel="stylesheet" type="text/css" href="./clock.css">
+    <link rel="stylesheet" type="text/css" href="./homePage.css">
+    <link rel="stylesheet" type="text/css" href="./clock2.css">
     <title>Home-Admin</title>
 </head>
 <body> 
