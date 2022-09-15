7dc78f2d3d5158e8dcfd2a9b1c83f9b6ec6591c4
bondoc-jd <jandenielbondocbondoc@gmail.com>
2022-09-15 13:55:37 +0800

    adjustd links

--- bondoc-jd-inventory/InventorySystem/addproduct.php
+++ bondoc-jd-inventory/InventorySystem/addproduct.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/employee2.css">
+    <link rel="stylesheet" href="css/employee2.css">
     <title>Supplier</title>
 </head>
 <body>
@@ -13,7 +13,7 @@
 <li><a class="active" href="./adminPage.php">Dashboard<i class="fa fa-home"></i></a></li>
         <li><a href="#news">input</a></li>
         <li><a href="#contact">input</a></li>
-        <li style="float:right"><a href="./forms/loginForm.php">Log out<i class="fa fa-arrow-right"></i></a></li>
+        <li style="float:right"><a href="forms/loginForm.php">Log out<i class="fa fa-arrow-right"></i></a></li>
 </ul>     
 <br/><br/>
 
@@ -22,7 +22,7 @@
     <div class="empContainer">
         <div class="inputEmployee">
          <div class="emp">
-                <form action="./forms/insertProduct.php" method="POST">
+                <form action="forms/insertProduct.php" method="POST">
                     <div class="inputEmp">
                         <input type="text" placeholder="Product ID" name="product_id" value="" required>
                     </div>
