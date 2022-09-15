fddd36c48e2c3433f01c2afd45f3b33c65da7310
unknown <patricksantos2627@gmail.com>
2022-09-15 16:51:11 +0800

    new and updated

---
+++ santos-p-RGW2(Responsive Gaming Website)/js/main.js
@@ -0,0 +1,7 @@
+let menu = document.querySelector('.menu-icon');
+let navbar = document.querySelector('.menu');
+
+menu.onclick = () => {
+    navbar.classList.toggle('active');
+    menu.classList.toggle('move');
+}
