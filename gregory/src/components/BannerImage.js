169a844636165b969bbb924dcc555f3ec5cc19ce
Angel Gregory Lansangan <aglansangan@gmail.com>
2022-09-17 13:04:32 +0800

    Tried REACT

---
+++ gregory/src/components/BannerImage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './master.css';
+function BannerImage(){
+    return(
+        <div>
+            <img className='banner' src="../images/banner.jpg" alt="BannerImage" />
+        </div>
+    )
+}
+export default BannerImage;
