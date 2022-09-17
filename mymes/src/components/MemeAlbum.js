dceff109d7d0dfb55412b75108ea71d7c7582ad9
Angel Gregory Lansangan <aglansangan@gmail.com>
2022-09-17 15:48:54 +0800

    tried to automatically add images from a folder

--- mymes/src/components/MemeAlbum.js
+++ mymes/src/components/MemeAlbum.js
@@ -1,16 +1,18 @@
 import React from "react";
-
 import PhotoEntry from "./PhotoEntry";
+import entries from './data/photo-entries';
 
 class MemeAlbum extends React.Component{
     render(){
+        console.log(entries);
         return(
         <div className="flex-container">
-            <PhotoEntry src="Horse.jpg" title="Reverse Centaur" caption="They're real! Even playing an Instrument!"/>
-            <PhotoEntry src="diet.jpg" title="Reverse Centaur" caption="They're real! Even playing an Instrument!"/>
-            <PhotoEntry src="badass.jpg" title="Reverse Centaur" caption="They're real! Even playing an Instrument!"/>
-            <PhotoEntry src="run.jpg" title="Reverse Centaur" caption="They're real! Even playing an Instrument!"/>
-            <PhotoEntry src="barbiepie.jpg" title="Reverse Centaur" caption="They're real! Even playing an Instrument!"/>
+            {
+                entries.itemlist.map((photo)=>{
+                    return <PhotoEntry key={photo.src} src={photo.src} title={photo.title} caption={photo.caption}/>
+                })
+            }
+            {/*<PhotoEntry src="Horse.jpg" title="Reverse Centaur" caption="They're real! Even playing an Instrument!"/>*/}
         </div>
         )
     }
