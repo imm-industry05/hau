3bf2b5e50507f9c939d4a1a45b690304a20ae80a
angelgregory <aglansangan@gmail.com>
2022-09-21 05:09:38 +0800

    Trying COnnection with MongoDB

---
+++ Memeic/server.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+
+app.get('/', (req, res)=>{
+    res.json({msg:"Hello"})
+})
+
+const URI = process.env.MONGODB_URL
+mongoose.connect(URI,{
+   
+   
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}), err =>{
+    if(err) throw err;
+    console.log('Connected to MongoDB')
+}
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+    console.log('Server is running on port', port)
+})
