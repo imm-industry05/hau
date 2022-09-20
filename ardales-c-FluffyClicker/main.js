fea3cdce1e72a4817d32934ce48aa53e84a73577
trollface939 <trollface939@yahoo.com.ph>
2022-09-21 00:03:17 +0800

    Alpha Test Build 0.0.2
    Added:
    Offline Production
    Formatting Numbers (Cannot Fix Formatting Issue)
    Balancing
    Tabs
    Styling

--- ardales-c-FluffyClicker/main.js
+++ ardales-c-FluffyClicker/main.js
@@ -1,43 +1,60 @@
+
+var saveGame = localStorage.getItem('goldMinerSave')
 var gameData = {
-    gold: 0,
-    goldPerClick: 1,
-    goldPerClickCost: 10
+  gold: 0,
+  goldPerClick: 1,
+  goldPerClickCost: 10,
+  lastTick: Date.now()
+}
+
+function update(id, content) {
+  document.getElementById(id).innerHTML = content;
 }
 
 function mineGold() {
-    gameData.gold += gameData.goldPerClick
-    document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
+  gameData.gold += gameData.goldPerClick
+  update("goldMined", gameData.gold + " Gold Mined")
 }
 
 function buyGoldPerClick() {
-    if (gameData.gold >= gameData.goldPerClickCost) {
-      gameData.gold -= gameData.goldPerClickCost
-      gameData.goldPerClick += 1
-      gameData.goldPerClickCost *= 2
-    }
+  if (gameData.gold >= gameData.goldPerClickCost) {
+    gameData.gold -= gameData.goldPerClickCost
+    gameData.goldPerClick += 1
+    gameData.goldPerClickCost *= 2
+    update("goldMined", gameData.gold + " Gold Mined")
+    update("perClickUpgrade", "Upgrade Pickaxe (Currently Level " + gameData.goldPerClick + ") Cost: " + gameData.goldPerClickCost + " Gold")
+  }
 }
 
-function buyGoldPerClick() {
-    if (gameData.gold >= gameData.goldPerClickCost) {
-      gameData.gold -= gameData.goldPerClickCost
-      gameData.goldPerClick += 1
-      gameData.goldPerClickCost *= 2
-      document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
-      document.getElementById("perClickUpgrade").innerHTML = "Upgrade Pickaxe (Currently Level " + gameData.goldPerClick + ") Cost: " + gameData.goldPerClickCost + " Gold"
-    }
+function tab(tab) {
+  document.getElementById("mineGoldMenu").style.display = "none"
+  document.getElementById("shopMenu").style.display = "none"
+  document.getElementById(tab).style.display = "inline-block"
 }
+tab("mineGoldMenu")
 
 var mainGameLoop = window.setInterval(function() {
-    mineGold()
+  diff = Date.now() - gameData.lastTick;
+  gameData.lastTick = Date.now()
+  gameData.gold += gameData.goldPerClick * (diff / 1000)
+  update("goldMined", gameData.gold + " Gold Mined")
 }, 1000)
 
 var saveGameLoop = window.setInterval(function() {
-    localStorage.setItem("goldMinerSave", JSON.stringify(gameData))
+  localStorage.setItem('goldMinerSave', JSON.stringify(gameData))
 }, 15000)
 
-var savegame = JSON.parse(localStorage.getItem("goldMinerSave"))
-if (savegame !== null) {
-  gameData = savegame
+function format(number, type) {
+	let exponent = Math.floor(Math.log10(number))
+	let mantissa = number / Math.pow(10, exponent)
+	if (exponent < 3) return number.toFixed(1)
+	if (type == "scientific") return mantissa.toFixed(2) + "e" + exponent
+	if (type == "engineering") return (Math.pow(10, exponent % 3) * mantissa).toFixed(2) + "e" + (Math.floor(exponent / 3) * 3)
 }
 
-if (typeof savegame.dwarves !== "undefined") gameData.dwarves = savegame.dwarves;
+update("perClickUpgrade", "Upgrade Pickaxe (Currently Level " + format(gameData.goldPerClick, "scientific") + ") Cost: " + format(gameData.goldPerClickCost, "scientific") + " Gold")
+
+if (typeof saveGame.gold !== "undefined") gameData.gold = saveGame.gold;
+if (typeof saveGame.goldPerClick !== "undefined") gameData.goldPerClick = saveGame.goldPerClick;
+if (typeof saveGame.goldPerClickCost !== "undefined") gameData.goldPerClickCost = saveGame.goldPerClickCost;
+if (typeof saveGame.lastTick !== "undefined") gameData.lastTick = saveGame.lastTick;
