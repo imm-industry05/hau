f7c97322ee0daa21479a5813513cea2e387fa903
unknown <atongoljr24@gmail.com>
2022-09-20 14:41:46 +0800

    Project updated 90% done

--- tongol-albert-full_responsive_multi_page_website_design/form-handler.php
+++ tongol-albert-full_responsive_multi_page_website_design/form-handler.php
@@ -1,4 +1,23 @@
 <?php
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$email = $_POST['email'];
+$number = $_POST['number'];
+$message = $_POST['message'];
+
+$email_from = 'info@yourwebsite.com';
+$email_subject = 'New Form Submission';
+$email_body =   "User FirstName: $firstname.\n".
+                "User LastName: $lastname.\n".
+                "User Email: $email.\n".
+                "User Message: $message.\n".;
+
+$to = 'atongoljr24@gmail.com';
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $email \r\n";
+mail($to,$email_subject,$email_body,$headers);
+header("Location: contact.html");
+
 
 
 ?>
