3d2e0f2543342058a962571db36fbe0f984ea2fc
angelgregory <aglansangan@gmail.com>
2022-09-21 15:15:10 +0800

    tried hash

--- Memeic/controllers/authCtrl.js
+++ Memeic/controllers/authCtrl.js
@@ -1,4 +1,4 @@
-const Users = require('../models/userModel')
+const Users = require('../models/useModel')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
@@ -6,8 +6,41 @@ const authCtrl = {
     register:async(req, res) => {
         try{
             const {fullname, username, email, password, gender} = req.body
-            console.log(req.body)
-            res.json({msg: 'Registered'})
+            let newUserName = username.toLowerCase().replace(/ /g, '')
+            
+            const user_name = await Users.findOne({username: newUserName})
+            if(user_name) return res.status(400).json({msg: "This username already exists"})
+
+            const user_email = await Users.findOne({email})
+            if(user_name) return res.status(400).json({msg: "This email already exists"})
+
+            if(password.length < 6)
+            return res.status(400).json({msg: "Password must be at least 6 characters."})
+     
+
+            const passwordHash = await bcrypt.hash(password, 12)
+            
+            const newUser = new Users({
+                fullname, username: newUserName, email, password: passwordHash, gender
+            })
+            console.log(newUser)
+
+            const access_token = createAccessToken({id: newUser._id})
+            const refresh_token = createRefreshToken({id: newUser._id})
+
+            res.cookie('refreshtoken', refresh_token, {
+                httpOnly: true,
+                path: '/api/refresh_token',
+                maxAge: 30*24*60*60*1000
+            })
+
+            res.json({msg: 'Register Success',
+                access_token,
+                user:{
+                    ...newUser._doc,
+                    password: ''
+                }
+            })
         }catch (err) {
             return res.status(500).json({msg: err.message})
         }
@@ -33,6 +66,14 @@ const authCtrl = {
             return res.status(500).json({msg: err.message})
         }
     }
+
+}
+
+const createAccessToken = (payload) =>{
+        return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'})
+}
+const createRefreshToken = (payload) => {
+    return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '30d'})
 }
 
 module.exports = authCtrl
