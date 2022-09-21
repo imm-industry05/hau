af021286c4ff2cc1cfef77589aa59675b592d91b
Neler Stephen Samonte <neyler.nss@gmail.com>
2022-09-21 22:09:48 +0800

    initial commit

---
+++ blog-greek-gods/create.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <title>BLOG: Greek Mythology</title>
+</head>
+<body>
+    <div class="container-fluid p-4 bg-warning">
+        <form method="POST">
+            <input type="text" placeholder="Title of the Blog" class="container-fluid form-control bg-dark text-warning text-center my-2">
+            <textarea class="form-control bg-dark text-warning py-5 my-4"></textarea>
+            <button class="btn btn-dark text-warning">Add Post</button>
+        </form>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"></script>
+</body>
+</html>
