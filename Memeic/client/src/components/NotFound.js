fae2b33d4f4e5f687efb59b3926758e326b50c26
angelgregory <aglansangan@gmail.com>
2022-09-21 09:09:39 +0800

    what if mislinked? 404

--- Memeic/client/src/components/NotFound.js
+++ Memeic/client/src/components/NotFound.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 const NotFound = () =>{
     return(
-        <div>
-            NotFound
+        <div className="position-relative" style={{
+            minHeight: 'calc(100vh - 70px)'
+        }}>
+            <h2 className='position-absolute text-secondary' style={{
+                top: '50%', left:'50%', transform: 'translate(-50%, -50%)'
+            }}>
+                404 ne, emu ne pipilit<br></br><br></br>:|
+           </h2>
         </div>
     )
 }
