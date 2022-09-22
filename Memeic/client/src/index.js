757aef17d10d9412370db0ee1731f1f35a1e9ce9
angelgregory <aglansangan@gmail.com>
2022-09-22 10:12:09 +0800

    Logo ng Memeic

--- Memeic/client/src/index.js
+++ Memeic/client/src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import DataProvider from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <DataProvider>
+    <App/>
+  </DataProvider>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- Memeic/client/src/pages/login.js
+++ Memeic/client/src/pages/login.js
@@ -13,22 +13,32 @@ const Login = () => {
     return(
         <div className="auth_page">
             <form>
-                <h3 className="text-uppercase">Memeic</h3>
+                <div className='Header'>
+                    <img src ="./Asset 3.svg" width={50}/>
+                    <h3 className="text-uppercase">Memeic</h3>
+                </div>
                 <div className="form-group">
                     <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
                     <input type="email" className="form-control" id="exampleInputEmail1" name="email"
                         aria-describedby="emailHelp" onChange={handleChangeInput} value={email}/>
                     
                     <small id="emailHelp" className="form-text">
-                        We'll never share your email with anyone else.
+                        Eka migaganaca san, eke pamie ing email mu.
                     </small>
                 </div>
                 <div className="forn-group">
                     <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
                     <input type="password" className="form-control" id="exampleInputPassword1" 
-                        onChange={handleChangeInput} value={password} name="password"/>
+                        onChange={handleChangeInput} value={password} name="password" />
                 </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
+
+                <button type="submit" className="btn btn-dark w-100"
+                    disabled={email && password? false:true}>
+                        Pasok
+                </button>
+                <p className="my-2">
+                    Ala kang Account? <Link to="/register" style={{color: "#9b5356"}}>Register ka pa</Link>
+                </p>
             </form>
         </div>
     )
