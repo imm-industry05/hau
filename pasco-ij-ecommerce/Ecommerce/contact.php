d49b20543af1428f616f75b752af95f418653a6e
pascojordan22 <pascojordan22@gmail.com>
2022-09-27 16:48:30 +0800

    Final Output

---
+++ pasco-ij-ecommerce/Ecommerce/contact.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title> Contact Us</title>
+        <link rel = "stylesheet" type="text/css" href="style3.css">
+
+        <link rel="preconenct" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <section class="contact">
+            <div class="contact-form">
+                <h1>Contact<span> Us</span></h1>
+                <p>Connect with me via phone: 09123523456 or email: pascojordan22@gmail.com </p>
+                <form action="">
+                    <input type="" placeholder="Your Name" required>
+                    <input type="email" name="email" id="" placeholder="E-mail" required>
+                    <input type="" placeholder="Write a Subject" required>
+                    <textarea name="" id="" cols="30" rows="10" placeholder="Your Message" required>
+                        
+                    </textarea>
+                    <input type="submit" name="" value="Submit" class="btn">
+                </form>
+            </div>
+
+            <div class="contact-img">
+                <img src="images/about.jpg">
+            </div>
+        </section>
+
+</body>
+</html>
