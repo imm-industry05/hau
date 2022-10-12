c0b3be15f9b341a20634a2f94c3876caad08d7f9
Neler Stephen Samonte <neyler.nss@gmail.com>
2022-10-12 12:08:03 +0800

    update

--- samonte-ns-blog/index.php
+++ samonte-ns-blog/index.php
@@ -9,35 +9,37 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
 
     <title>BLOG: Greek Mythology</title>
 </head>
 <body>
-    <div class="container-fluid p-4 bg-warning text-center text-dark h1">
-        <strong>BLOG: GODS AND GODDESSES IN GREEK MYTHOLOGY</strong>
-        <div class="container mt-4">
-            <?php if(isset($_REQUEST['info'])){?>
-                <?php if ($_REQUEST['info'] == "added"){?>
-                    <div class="alert alert-success h4" role="alert">
-                        <strong>New blog content has been successfully added!</strong>
-                    </div>
-                <?php } else if($_REQUEST['info'] == "updated"){ ?>
-                    <div class="alert alert-success h4" role="alert">
-                        <strong>New blog content has been successfully edited!</strong>
-                    </div>
-                <?php } else if($_REQUEST['info'] == "deleted"){ ?>
-                    <div class="alert alert-danger h4" role="alert">
-                        <strong>Blog content has been deleted!</strong>
-                    </div>
+    <div class="background">
+        <div class="container-fluid p-4 bg-warning text-center text-dark h1">
+            <strong>BLOG: GODS AND GODDESSES IN GREEK MYTHOLOGY</strong>
+            <div class="container mt-4">
+                <?php if(isset($_REQUEST['info'])){?>
+                    <?php if ($_REQUEST['info'] == "added"){?>
+                        <div class="alert alert-success h4" role="alert">
+                            <strong>New blog content has been successfully added!</strong>
+                        </div>
+                    <?php } else if($_REQUEST['info'] == "updated"){ ?>
+                        <div class="alert alert-success h4" role="alert">
+                            <strong>Blog content has been successfully edited!</strong>
+                        </div>
+                    <?php } else if($_REQUEST['info'] == "deleted"){ ?>
+                        <div class="alert alert-danger h4" role="alert">
+                            <strong>Blog content has been deleted</strong>
+                        </div>
+                    <?php }?>
                 <?php }?>
-            <?php }?>
 
-            <div class="text-center">
-                <a href="create.php" class="btn btn-dark text-warning">Create New Blog</a>
+                <div class="text-center">
+                    <a href="create.php" class="btn btn-dark text-warning">Create New Blog</a>
+                </div>
             </div>
-        </div>
-    </div>    
+        </div>    
         
         <div class="row">
             <?php foreach($query as $q){?>
@@ -52,6 +54,7 @@
                 </div>
             <?php }?>
         </div>
+    </div>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js"></script>
