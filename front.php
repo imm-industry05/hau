14fa48b08c02a1a6c75b25874e391e6046851aaf
Micaella Nicole Gudez <gudez.coycoy@gmail.com>
2022-10-13 19:43:05 +0800

    feat: created home page

---
+++ front.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="css/style.css">
+</head>
+<body style="height:100%; background:url(img/logobg.png); background-repeat:no-repeat; background-position:center; background-size:cover; background-attachment:fixed;">
+<div class="front-container">
+
+   <button><p><a href="login_form.php" class="btn buttton-login">login</a></p> </button>
+   <button><p><a href="register_form.php" class="btn button-register">register</a></p> </button>
+
+</div>
+</body>
+</html>
